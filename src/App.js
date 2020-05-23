88c53bc9c480542c8ea572af4ee1a0a677b8bf8b
ksso730 <ksso7300@gmail.com>
2020-05-23 10:26:45 +0900

    add react function project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div>
+      <h1>이름 : </h1>
+    <h1>성별 : </h1>
+<h1>나이 : </h1>
+</div>
+  );
+}
+
+export default App;
